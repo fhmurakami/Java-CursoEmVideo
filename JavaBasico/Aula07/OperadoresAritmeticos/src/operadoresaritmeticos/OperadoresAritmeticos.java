86ecd23b90a14b50443a79b88d9d243450a0e6e4
Felipe Murakami <fhmurakami@gmail.com>
2018-03-13 21:07:49 -0300

    Add files via upload
    Aula 07 do curso básico de Java

---
+++ JavaBasico/Aula07/OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author Felipe
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /* int n1 = 3;
+         * int n2 = 5;
+         * float m = (n1 + n2)/2;
+         * System.out.println("A média é igual a " + m);
+         */
+        
+        /* int numero = 5;
+         * int valor = 5 + numero++;
+         * System.out.println(valor);
+         */
+        
+        /* int x = 4;
+         *x *= 2; // x = x * 2
+         *System.out.println(x);
+         */
+        
+        /* float v = 8.4f;
+         * int ar = (int) Math.round(v);
+         * System.out.println(ar);
+         */
+        
+        /*double aleatorio = Math.random();
+        int n = (int) (5 + aleatorio * (10 - 5)); // Gera números entre 5 e 10
+//        System.out.println(aleatorio);
+        System.out.println(n);*/
+        int n1 = 2, n2 = 5;
+        double resultado = n2/n1;
+        System.out.println(resultado);
+    }
+    
+}
